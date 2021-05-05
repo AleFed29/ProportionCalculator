aae3052241b2f21d68c87345a2f6fd827d5eea27
Tommaso Tosca <tommaso.tosca.studenti@isii.it>
2021-05-05 16:03:28 +0200

    Adding image in interface
    Adding image in tkinter.

--- Progetto/frontend_interface.py
+++ Progetto/frontend_interface.py
@@ -30,8 +30,11 @@ def createForm(img_name, width, height, XPOS, YPOS, image, data):
     #I will show the image here
     frame_for_image = tkinter.Frame(frame, bg='white')
     frame_for_image.place(relx = 1/6, rely = 1/3, relwidth = 1/3,relheight = 1/3)
-    #code for the image
-
+    #code for the image     
+    canvas = Canvas(frame_for_image, width = 300, height = 300)      
+    canvas.pack()      
+    img = cv2.imread(img_name,0) #PhotoImage(file = img_name)      
+    canvas.create_image(20,20, anchor=NW, image=img)    
 
     #I will insert the table here
     frame_for_table = tkinter.Frame(frame, bg='gray')
