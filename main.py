8da3c0f1b594b39f123773b2dc678fe489ab3548
AleFed29 <75224168+AleFed29@users.noreply.github.com>
2021-05-12 16:17:04 +0200

    no time

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+import importlib
+importlib.import_module("proportion_calculator")
+importlib.import_module("frontend_interface")
+#from proportion_calculator import *
+import frontend_interface as frontend
+
+width = 500
+height = 500
+XPOS = 200
+YPOS = 100
+data_fake = [
+    ['id', 'Picture','Rectangles', 'Trapezoids', 'Cavalieri-Simpson(Parabolas)', 'error_R', 'error_T', 'eror_P', 'time_R','time_T','time_P', 'time-quality-ratio_R',
+     'time-quality-ratio_T', 'time-quality-ratio_P'], #tuples with data
+    [1, 'Gioconda', 0.055, 0.070, 0.089, 0.006, 0.03, 0.001, 0.04,0.02, 0.01, 0.09, 0.05, 0.06] #tuples with data
+    ]
+
+def Process(img_name, width, height, XPOS, YPOS):
+    #fill with functions from ProportionCalculator
+    return True
+
+if __name__ == "__main__":
+    frontend.createForm("Gioconda.jpg", width, height, XPOS, YPOS, data_fake)
+    #call threshold and erosion 
+    #Process("Gioconda.jpg",width, height, XPOS, YPOS, data_fake)
+    #Process("SalvatorMundi.jpg",width, height, XPOS, YPOS, data_fake)
+    #Process("DamaConErmellino.jpg",width, height, XPOS, YPOS, data_fake)
+    os.system("PAUSE")
