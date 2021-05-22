8fd6e88b823a233e5dad122088f9877aa73a5721
AleFed29 <75224168+AleFed29@users.noreply.github.com>
2021-05-22 13:06:35 +0200

    Salvator and Dama work

--- Progetto/modules/image_transformer.py
+++ Progetto/modules/image_transformer.py
@@ -3,50 +3,40 @@ import numpy as np
 
 
 def threshold(img_name,keyval):
-  #name of the new image
-    name_split = str(img_name).split(".")
-    begin = ""
-    for n in range(len(name_split)-1):
-        begin = begin + name_split[n]
-    name = "." + begin + "_bianco_e_nero." + name_split[len(name_split)-1]
+    #name of the new image
+    name_split = img_name.split(".")
+    name = name_split[0] + "_bianco_e_nero." + name_split[1]
     #oldname_bianco_e_nero.format
-    img = cv2.imread(img_name, 0) #0 grayscale, 1 coloured, -1 unchanged
+    img = cv2.imread(img_name, 0)
+    
     cv2.imshow("prov", img)
-    mainname = begin.split('/')[2]
-
-    if mainname == "SalvatorMundi":
-        #black if color Average of RGB values < keyval else white
-        ret, threshold_img = cv2.threshold(img, keyval, 255, cv2.THRESH_BINARY)
-        threshold_img  = cv2.erode(threshold_img , None, iterations=1)
-
-    elif mainname == "DamaConErmellino":
-        ret, threshold_img = cv2.threshold(img, keyval, 255, cv2.THRESH_BINARY)
-        threshold_img  = cv2.erode(threshold_img , None, iterations=2)
-
-    elif mainname == "Gioconda":
-        ret, threshold_img = cv2.threshold(img, keyval, 255, cv2.THRESH_BINARY_INV)
-        threshold_img  = cv2.erode(threshold_img , None, iterations=3)
-        threshold_img = cv2.bitwise_not(threshold_img)
-        threshold_img  = cv2.erode(threshold_img , None, iterations=3)
-        threshold_img = cv2.bitwise_not(threshold_img)
-    cv2.imshow(mainname,threshold_img)
     cv2.waitKey(0)
+    #black if color Average of RGB values < keyval else white
+    #treshold_img = cv2.threshold(img, keyval, 255, cv2.THRESH_BINARY)[1] #ret, threshold = ... 
+    ret, threshold_img = cv2.threshold(img, keyval, 255, cv2.THRESH_BINARY)
+    #treshold_img = np.array(cv2.threshold(img, keyval, 255, cv2.THRESH_BINARY)[1])
+    
+    #threshold_img = threshold_img
+    cv2.imshow("Prova", threshold_img)
+    cv2.waitkey(0)
+    #cv2.imshow('original', img)
+    #cv2.imshow('B&W', threshold)
+    #cv2.waitkey(0)
+    #cv2.destroyAllWindows()
     result = cv2.imwrite(name, threshold_img)
     if result:
         print("File saved successfully.")
     else:
         print("Error in saving file.")
-    cv2.destroyAllWindows()
     return name
 
 
-
-#def erosion(img_name, xKernel, yKernel, iterations_number):
+def erosion(img_name, xKernel, yKernel, iterations_number):
     name_split = img_name.split(".")
     name = name_split[0] + "_finito." + name_split[1]
     img = cv2.imread(img_name,0)
-    #kernel = np.ones((xKernel, yKernel),np.uint8) non expedit
-    erosion = cv2.erode(img,None,iterations = iterations_number)
+    kernel = np.ones((xKernel, yKernel),np.uint8)
+    erosion = cv2.erode(img,kernel,iterations = iterations_number)
 
     result = cv2.imwrite(name, erosion)
     if result:
@@ -55,6 +45,5 @@ def threshold(img_name,keyval):
         print("Error in saving file.")
     return name
 
-def ThresholdedPictureName(img_name, keyval):
-    #return erosion(threshold(img_name, keyval),xKernel, yKernel, iterations_number) 
-    return threshold(img_name, keyval)
+def ThresholdedPictureName(img_name, keyval,xKernel, yKernel, iterations_number):
+    return erosion(threshold(img_name, keyval),xKernel, yKernel, iterations_number) 
--- Progetto/modules/proportion_calculator.py
+++ Progetto/modules/proportion_calculator.py
@@ -1,7 +1,8 @@
 import os
 import cv2 as cv
 import modules.image_transformer as transformer
-from datetime import datetime
+import datetime
+
 
 global KEYVAL
 global xKERNEL
@@ -10,76 +11,49 @@ global ITERATIONS_NUMBER
 
 #creates the image with the name composed by the methods, then it returns the name
 def iswhite(pixelRGB):
-    return(pixelRGB == 255)
-    #return int(pixelRGB[0]) == 255 and int(pixelRGB[1]) == 255 and int(pixelRGB[2]) == 255
+    return pixelRGB[0] == 255 and pixelRGB[1] == 255 and pixelRGB[2] == 255 
 
 def isblack(pixelRGB):
-    return pixelRGB == 0
-    #return pixelRGB[0] == 0 and pixelRGB[1] == 0 and pixelRGB[2] == 0
+    return pixelRGB[0] == 0 and pixelRGB[1] == 0 and pixelRGB[2] == 0
    
 
-def FunctionsFromArea(img_name, keyval):
-    img = cv.imread(str(transformer.ThresholdedPictureName(img_name, keyval)), 0)
+def FunctionsFromArea(img_name, keyval,xKernel, yKernel, iterations_number):
+    img = cv.imread(transformer.ThresholdedPictureName(img_name, keyval,xKernel, yKernel, iterations_number))
     # get dimensions of image
     dimensions = img.shape
     # height, width, number of channels in image
-    height = dimensions[0]-1
-    img_width = dimensions[1]-1 #it would be reversed
-    print(img_width,'x',height)
-    dxfunc = DxFunc(img, ystart=height, width=img_width)
+    height = dimensions[0]
+    img_width = dimensions[1]
     sxfunc = SxFunc(img, ystart=height, width=img_width)
+    dxfunc = DxFunc(img, ystart=height, width=img_width)
     return sxfunc, dxfunc
 
-#immaginare l'immagine ruotata di 90 gradi verso sistra
-def DxFunc(img, ystart, width):#function on the right
-    function_dx = dict() #f
-    xstart = int(width/2)
-    x = abs(int(ystart)-1)
-    lastvalue = 20
-    while lastvalue > 1: #quando non trova punti bianchi, la figura è finita
-        for i in range(int(width/2)):
-            y = abs(xstart - i) #y parte da width/2 e scende, x fissa 
-            xrel = int(abs(ystart-x)) #xrel parte da 1 e va avanti dopo ogni ciclo for
-            yrel = abs(y + int(width/2))
-            #print(xrel, yrel)
-            #yrel,x eppure a quanto pare l'asse 0 è l'altezza
-            if(abs(x) < ystart and abs(yrel) < int(width/2)):
-                if img[x,yrel] > 200: #coord pixel, se il pixel è bianco (non nero, per sfumature)
-                    break
-        punti =  list(function_dx.keys())   
-        y = 2 if y < 2 and len(punti) < 500 else y  #se almeno 500 punti e un pixel arriva ad uno, allora esce.      
-        function_dx[xrel] = y #f(xrel) = y
-        lastvalue = y #ordinata dell'ultimo pixel bianco trovato
-        x = x - 1 #punto alla nuova x
-        y = xstart #ritorna su   
-    return function_dx    
+def SxFunc(img, ystart, width): #function on the left
+    function_sx = dict()
+    while ystart > 0:
+        xstart = int(width/2)
+        color = img[xstart, ystart]
+        while not isblack(color):
+            xstart = xstart-1
+            color = img[xstart, ystart]
+        function_sx[ystart] = int(width/2) - xstart #ribaltato
+        ystart = ystart - 1
+    return function_sx    
 
-#immaginare l'immagine ruotata di 90 gradi verso destra
-def SxFunc(img, ystart, width):#function on the left
-    function_sx = dict() #f
-    #xstart = 0
-    x = abs(int(ystart)) - 1 #metti for per spostare le x
-    lastvalue = 20
-    while lastvalue > 1: #quando non trova punti bianchi, la figura è finita
-        for i in range(int(width/2)):
-            #y = i #y parte da 0 e sale, x fissa 
-            xrel =  int(abs(abs(x)-abs(ystart))) #xrel parte da 1 e va avanti dopo ogni ciclo for
-            yrel = int(abs(width/2 - i)) #da width/2 a scendere, perché parti sempre dall'alto
-            #yrel,xrel eppure a quanto pare l'asse 0 è l'altezza
-            if (abs(x) < ystart and abs(yrel) < int(width/2)):
-                if img[xrel,yrel] > 200: #coord pixel, se il pixel è bianco (non nero, per sfumature)
-                    break    
-        punti =  list(function_sx.keys())   
-        yrel = 2 if yrel < 2 and len(punti) < 500 else yrel  #se almeno 500 punti e un pixel arriva ad uno, allora esce. 
-        function_sx[xrel] = yrel #f(xrel) = yrel
-        lastvalue = yrel #ordinata dell'ultimo pixel bianco trovato
-        x = x - 1
-        #y = 0 #xstart   
-    return function_sx
+def DxFunc(img, ystart, width): #function on the left
+    function_dx = dict()
+    while ystart > 0:
+        xstart = int(width/2)
+        color = img[xstart, ystart]
+        while not isblack(color):
+            xstart = xstart + 1
+            color = img[xstart, ystart]
+        function_dx[ystart] = int(width/2) - xstart #ribaltato
+        ystart = ystart - 1
+    return function_dx
 
-            
 def GetValueFrom(x, dictionary):
-    return dictionary[x] if x in dictionary else 0
+    return dictionary[x] if x in dictionary else None
 
 #img_name = ThresholdedPictureName()
 def Xs(start, end, n):
@@ -93,12 +67,10 @@ def Rectangles(f, start, end, n):
     sum = 0
     xs = Xs(start, end, n)
     for x in xs:
-        y = GetValueFrom(x,f)
-        print(x, y)
-        sum = sum + y
+        sum = sum + GetValueFrom(x,f)
     h = abs(end - start)/n
     errmax = derMax(f)*h*(end-start)/2
-    return [h*sum, errmax]
+    return h*sum, errmax
 
 def Trapezoids(f, start, end, n):
     sum = 0
@@ -112,7 +84,7 @@ def Trapezoids(f, start, end, n):
             sum = sum + 2 * GetValueFrom(x,f)
         i = i + 1
     errmax = derIIMax(f) * h*h*(end-start)/12        
-    return [h*sum/2, errmax]        
+    return h*sum/2, errmax        
 
 def Parabolas(f, start, end, n):
     sum = 0
@@ -128,17 +100,16 @@ def Parabolas(f, start, end, n):
             sum = sum + 4 * GetValueFrom(x,f)
         i = i + 1
         errmax = derIVMax(f)*(h**4)*(end-start)/2880
-    return [h*sum/3, errmax] 
+    return h*sum/3, errmax 
 
 def TestRectangles(f,err):
-    data_keys = list(f.keys())
-    kmin = data_keys[0]
-    kmax = data_keys[len(data_keys)-1] #they are already ordered
+    kmin = f.keys()[0]
+    kmax = f.keys()[len(f.keys())-1] #they are already ordered
     i = 1
     J2 = Rectangles(f, kmin, kmax, i)
     while True:
         J1 = Rectangles(f,kmin, kmax, 2*i)
-        diff = abs(float(J1[0]) - float(J2[0]))
+        diff = abs(J1[0] - J2[0])/15
         if diff < err:
             return J1
         J2 = J1
@@ -146,33 +117,31 @@ def TestRectangles(f,err):
 
 
 def TestTrapezoids(f, err):
-    data_keys = list(f.keys())
-    kmin = data_keys[0]
-    kmax = data_keys[len(data_keys)-1] #they are already ordered
+    kmin = f.keys()[0]
+    kmax = f.keys()[len(f.keys())-1] #they are already ordered
     i = 1
     J2 = Trapezoids(f, kmin, kmax, i)
     while True:
         J1 = Trapezoids(f, kmin, kmax, 2*i)
-        diff = abs(float(J1[0]) - float(J2[0]))/3
+        diff = abs(J1[0] - J2[0])/15
         if diff < err:
             return J1
         J2 = J1
         i = 2*i  
 
 def TestParabolas(f,err):
-    data_keys = list(f.keys())
-    kmin = data_keys[0]
-    kmax = data_keys[len(data_keys)-1] #they are already ordered
+    kmin = f.keys()[0]
+    kmax = f.keys()[len(f.keys())-1] #they are already ordered
     i = 1 
     J2 = Parabolas(f, kmin, kmax, i)
     while True:
         J1 = Parabolas(f, kmin, kmax, 2*i)[0]
-        diff = abs(float(J1[0]) - float(J2[0]))/15
+        diff = abs(J1[0] - J2[0])/15
         if diff < err:
             return J1
         J2 = J1
         i = 2*i  
-           
+            
 
 def derMax(f):
     max = 0
@@ -180,7 +149,7 @@ def derMax(f):
     for x in f:
         if x<1: continue
         ysucc = GetValueFrom(x,f) #f(1), f(2),...
-        if yprev != 0 and ysucc != 0:
+        if yprev != None and ysucc != None:
             yo = abs(ysucc - yprev) #|f'(x)|
             if max < yo:
                 max = yo
@@ -194,7 +163,7 @@ def derIIMax(f):
     for x in f:
         if x < 2: continue
         ysucc = GetValueFrom(x,f) #f(2), f(3), ...
-        if yprev != 0 and ysucc != 0:
+        if yprev != None and ysucc != None:
             intsucc = ysucc - yprev #f'(1), #f'(2)
             yo = abs(intsucc - intprev) #|f''(0)|, |f''(1)|, ...
             if max < yo:
@@ -209,12 +178,12 @@ def derIVMax(f):
     ys = GetValueFrom(0,f) #y start
     ym = GetValueFrom(1,f) #y in the middle
     yf = GetValueFrom(2,f) #y at the end
-    intIIprev = (yf-ym)-(ym - ys)  #f''(0)
+    intIIprev = (yf-ym)-(ym - ys) if ys != None and ym != None and yf != None else 0 #f''(0)
 
     ys = ym
     ym = yf
     yf = GetValueFrom(3,f)
-    intIIsucc = (yf-ym)-(ym - ys)  #f''(1) 
+    intIIsucc = (yf-ym)-(ym - ys) if ys != None and ym != None and yf != None else 0 #f''(1) 
     
     intIVprev = intIIsucc - intIIprev #f'''(0)
     intIIprev = intIIsucc
@@ -222,7 +191,7 @@ def derIVMax(f):
     ys = ym
     ym = yf
     yf = GetValueFrom(4,f)
-    intIIsucc = (yf-ym)-(ym - ys) #f''(2)
+    intIIsucc = (yf-ym)-(ym - ys) if ys != None and ym != None and yf != None else 0 #f''(2)
 
     intIVsucc = intIIsucc - intIIprev #f'''(1)
     max = abs(intIVsucc - intIVprev) #|f(IV)(0)|
@@ -234,7 +203,7 @@ def derIVMax(f):
         ys = GetValueFrom(x,f) #y start
         ym = GetValueFrom(x+1,f) #y in the middle
         yf = GetValueFrom(x+2,f) #y at the end
-        if ys != 0 and ym != 0 and yf != 0: continue
+        if ys != None and ym != None and yf != None: continue
         intIIsucc = (yf-ym)-(ym - ys) #f''(3), f''(4),...
         intIVsucc = intIIsucc - intIIprev #f'''(2)
         yo = abs(intIVsucc - intIVprev)#|f(IV)(1)|
@@ -244,49 +213,33 @@ def derIVMax(f):
         intIVprev = intIVsucc
     return max
 
-def CreateTableData(img_name,KEYVAL):
-    FUNCTION_1, FUNCTION_2 = FunctionsFromArea(img_name, KEYVAL) #method to have the function
-    
-    img = cv.imread(img_name,1)
-    # get dimensions of image
-    #dimensions = img.shape
-    # height, width, number of channels in image
-    area = img.size
-    
-    print(len(FUNCTION_1))
-    print(len(FUNCTION_2)) #capire perché funzione 1 ha più punti. Per il resto, ok tranne elaborazione.
-    #for key in FUNCTION_1:
-    #    print(key, FUNCTION_1[key])
-    #for key in FUNCTION_2:
-    #    print(key, FUNCTION_2[key])
-    return
-    err = 0.01 #max error
-    print("Inizio")
-    time_s = datetime.now()
+def CreateTableData(img_name):
+    KEYVAL = 0
+    xKERNEL = 0
+    yKERNEL = 0
+    ITERATIONS_NUMBER=0
+    FUNCTION_1, FUNCTION_2 = FunctionsFromArea(img_name, KEYVAL, xKERNEL, yKERNEL, ITERATIONS_NUMBER) #method to have the function
+    err = 0.01
+    time_s = datetime()
     A1 = TestRectangles(FUNCTION_1,err)
-    print("lista", A1)
-    return
     A2 = TestRectangles(FUNCTION_2,err)
     rectangles = [A1[0] + A2[0], (A1[1]+A2[1])/2]
-    time_rect = (datetime.now() - time_s).microseconds/1000  #it takes a little time, i don't consider it
-    print("Rettangoli")
-    return
-    time_s = datetime.now()# boh. function missing required argument 'year' (pos 1
+    time_rect = time_s - datetime() #it takes a little time, i don't consider it
+    time_s = datetime()
     A1 = TestTrapezoids(FUNCTION_1,err)
     A2 = TestTrapezoids(FUNCTION_2,err)
     trapezoids = [A1[0] + A2[0], (A1[1]+A2[1])/2]
-    time_trap = (datetime.now() - time_s).microseconds/1000 #milliseconds, not microseconds
-
-    time_s = datetime.now()
+    time_trap = time_s - datetime()
+    time_s = datetime()
     A1 = TestParabolas(FUNCTION_1,err)
     A2 = TestParabolas(FUNCTION_2,err)
     parabolas = [A1[0] + A2[0], (A1[1]+A2[1])/2]
-    time_par = (datetime.now - time_s).microseconds/1000 
+    time_par = time_s - datetime() 
     return [
-        ["N","Method Name", "Value", "err","proportion(%)", "time (ms)", "coeff.time-err"],
-        [1, "Rectangles", rectangles[0], rectangles[1],round(rectangles[0]*100/area,2), time_rect, time_rect*rectangles[1]],
-        [2, "Trapezoids",trapezoids[0], trapezoids[1],round(trapezoids[0]*100/area,2), time_trap, time_trap*trapezoids[1]],
-        [3, "Parabolas",parabolas[0], parabolas[1],round(parabolas[0]*100/area,2), time_par, time_par*parabolas[1]]
+        ["N", "Value", "err", "time", "coeff.time-err"],
+        [1, rectangles[0], rectangles[1], time_rect, time_rect*rectangles[1]],
+        [2, trapezoids[0], trapezoids[1], time_trap, time_trap*trapezoids[1]],
+        [3, parabolas[0], parabolas[1], time_par, time_par*parabolas[1]]
     ]
     #it takes the function -> three methods, data collected with datetime
 
